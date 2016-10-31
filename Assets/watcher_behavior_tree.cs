7ef9aa9dcf9876eda85058a65797764c10d8f4f1
Joshua Westbrook <profdeadpool@gmail.com>
2016-10-30 22:48:30 -0400

    Added watchers having a new behavior on left click. Still needs animation though

--- Assets/watcher_behavior_tree.cs
+++ Assets/watcher_behavior_tree.cs
@@ -12,6 +12,7 @@ public class watcher_behavior_tree : MonoBehaviour {
     // Use this for initialization
     void Start()
     {
+		
         dancers = GameObject.FindGameObjectsWithTag("dancer");
         behaviorAgent = new BehaviorAgent(this.BuildTreeRoot());
         BehaviorManager.Instance.Register(behaviorAgent);
@@ -21,7 +22,12 @@ public class watcher_behavior_tree : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
-       
+		if (Input.GetMouseButtonDown(0)) {
+			behaviorAgent.StopBehavior();
+			behaviorAgent = new BehaviorAgent(this.BuildTreeRoot2());
+			BehaviorManager.Instance.Register(behaviorAgent);
+			behaviorAgent.StartBehavior();
+		}
     }
 
     protected Node ST_ApproachAndLook(Transform target)
@@ -29,6 +35,12 @@ public class watcher_behavior_tree : MonoBehaviour {
         Val<Vector3> position = Val.V(() => target.position);
         return new Sequence(participant.GetComponent<BehaviorMecanim>().Node_GoToUpToRadius(position,new Val<float>(5)), participant.GetComponent<BehaviorMecanim>().Node_OrientTowards(position), new LeafWait(1000));
     }
+
+	protected Node ST_ThrowBallAt(Transform target) {
+		//this should animate a ball being thrown at the dancer's transform
+		Val<Vector3> position = Val.V(() => target.position);
+		return new Sequence(participant.GetComponent<BehaviorMecanim>().Node_GoToUpToRadius(position,new Val<float>(5)), participant.GetComponent<BehaviorMecanim>().Node_OrientTowards(position), new LeafWait(1000));
+	}
     
     protected Node BuildTreeRoot()
     {
@@ -39,4 +51,14 @@ public class watcher_behavior_tree : MonoBehaviour {
         Node root = new DecoratorLoop(roaming); 
         return root;
     }
+
+	protected Node BuildTreeRoot2()
+	{
+		Node roaming = new DecoratorLoop(
+			new Sequence(
+				this.ST_ThrowBallAt(this.wander1)
+			));
+		Node root = new DecoratorLoop(roaming); 
+		return root;
+	}
 }
