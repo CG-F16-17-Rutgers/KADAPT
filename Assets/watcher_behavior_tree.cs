de3df1301a708a9375af9f9ca6db6b8d059e57eb
Michael Alweis <alweism@gmail.com>
2016-10-30 22:39:17 -0400

    more trimming

--- Assets/watcher_behavior_tree.cs
+++ Assets/watcher_behavior_tree.cs
@@ -29,33 +29,7 @@ public class watcher_behavior_tree : MonoBehaviour {
         Val<Vector3> position = Val.V(() => target.position);
         return new Sequence(participant.GetComponent<BehaviorMecanim>().Node_GoToUpToRadius(position,new Val<float>(5)), participant.GetComponent<BehaviorMecanim>().Node_OrientTowards(position), new LeafWait(1000));
     }
-    protected Node ST_Watch()
-    {
-        int duration = 0;
-        bool watch = false; ;
-        Val<Vector3> position = null;
-        foreach ( GameObject dancer in dancers)
-        {
-            Animator anim = dancer.GetComponent<Animator>();
-            bool isDancing = anim.GetBool("B_Breakdance");
-            watch = (Vector3.Distance(dancer.transform.position, participant.transform.position) < 5) && isDancing;
-            if (watch)
-            {
-                position = Val.V(() => dancer.transform.position);
-                duration = 1000;
-                break;
-            }
-        }
-        if (position == null)
-        {
-            position = participant.transform.forward;
-        }
-        Func<bool> watching = () => (
-          watch
-        );
-        Node trigger = new LeafAssert(watching);
-        return new Sequence(trigger, participant.GetComponent<BehaviorMecanim>().Node_OrientTowards(position),new LeafWait(duration));
-    }
+    
     protected Node BuildTreeRoot()
     {
         Node roaming = new DecoratorLoop(
